a6dde74540bf5755e090232ab754ef3f00d45b4b
Synkied <synkx@hotmail.fr>
2016-02-01 21:52:55 +0100

    Added picture and text to the APP

---
+++ AnswerBall/MainViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MainViewController.swift
+//  AnswerBall
+//
+//  Created by Quentin Lathière on 1/28/16.
+//  Copyright © 2016 Quentin Lathière. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
